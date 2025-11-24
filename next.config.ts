b3002ec3320a0d831ffecb99c5b329746dd310fb
viadel13 <shababizetre@gmail.com>
2025-11-24 10:43:30 +0100

    jhj

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,18 @@
-const withPWA = require("next-pwa")({
+const withPWA = require("@ducanh2912/next-pwa").default({
     dest: "public",
-    register: true,
-    skipWaiting: true,
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
     disable: process.env.NODE_ENV === "development",
+    workboxOptions: {
+        disableDevLogs: true,
+    },
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    // Autres configurations de Next.js si nécessaire
 };
 
 module.exports = withPWA(nextConfig);
